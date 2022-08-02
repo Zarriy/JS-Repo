310f4e163dcfc1c2c0e3019d0f351021a80a49e0
zarriy <zarriy@outlook.com>
2022-08-02 12:52:55 +0500

    finished Getting Joke using Primises

--- Jokes-page/script.js
+++ Jokes-page/script.js
@@ -1,6 +1,25 @@
 const jokeDiv = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
 
-const requestJoke = new XMLHttpRequest();
-requestJoke("GET", "https://icanhazdadjoke.com/");
-requestJoke.send();
-console.log(requestJoke.responseText);
+// const requestJoke = new XMLHttpRequest();
+// requestJoke.open("GET", `https://icanhazdadjoke.com/`);
+// requestJoke.send();
+// console.log(requestJoke.responseText);
+
+// requestJoke.addEventListener("load", function () {
+//   console.log(this.responseText);
+//   const data = JSON.parse(this.responseText);
+// });
+function getAJoke() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  })
+    .then(function (res) {
+      console.log(res);
+      return res.json();
+    })
+    .then(function (data) {
+      jokeDiv.textContent = data.joke;
+    });
+}
+btn.addEventListener("click", getAJoke);
